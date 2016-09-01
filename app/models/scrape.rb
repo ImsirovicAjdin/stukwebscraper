533b23a8d33b6c4eecdde7f685d6043f87e93ccd
ajdin <ajdika@live.com>
2016-09-01 08:53:40 +0000

    add encoding utf8 conversion, and save_movie method to save to db

--- app/models/scrape.rb
+++ app/models/scrape.rb
@@ -16,10 +16,27 @@ class Scrape
             self.release_date = doc.at("/html/body[@class='body  ']/div[@class='body_main container']/div[@id='main_container']/div[@id='mainColumn']/section[@class='panel panel-rt panel-box movie_info media']/div[@class='media-body']/div[@class='panel-body content_body']/div[@class='info']/div[@class='col-sm-19 col-xs-14 text-left'][3]/time").text.to_date
             self.runtime = doc.at("/html/body[@class='body  ']/div[@class='body_main container']/div[@id='main_container']/div[@id='mainColumn']/section[@class='panel panel-rt panel-box movie_info media']/div[@class='media-body']/div[@class='panel-body content_body']/div[@class='info']/div[@class='col col-sm-19 col-xs-14 text-left'][4]/time").text
             self.synopsis = doc.css("#movieSynopsis").text
+            s = doc.css('#movieSynopsis').text
+			if ! s.valid_encoding?
+				s = s.encode("UTF-16be", :invalid=>:replace, :replace=>"?").encode('UTF-8')
+			end
+			self.synopsis = s
             return true
         rescue Exception => e
             self.failure = "Something went wrong with the scrape"
         end
     end
-    
+	def save_movie
+		movie = Movie.new(
+			title: self.title,
+			hotness: self.hotness,
+			image_url: self.image_url,
+			synopsis: self.synopsis,
+			rating: self.rating,
+			genre: self.genre,
+			director: self.director,
+			runtime: self.runtime
+			)
+		movie.save
+	end    
 end
